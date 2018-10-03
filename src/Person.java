8a37e15159db463a231b0928232ecb9135ab18e7
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-10-03 12:18:25 -0400

    Updates to Person Manager App

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+	
+	
+	private String first;
+	private String last;
+	
+	
+
+	
+//	•	Create a class named Person with these constructors and methods:
+//		public Person(String first, String last)
+//		public String getFirstName()
+//		public void setFirstName(String first)
+//		public String getLastName()
+//		public void setLastName()
+//		The Person class should override the toString() method so it returns the first name and last name in this format:
+//		Name: Frank Jones
+
+}
---
+++ src/PersonManagerApp.java
@@ -0,0 +1,9 @@
+
+public class PersonManagerApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
