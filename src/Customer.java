f32b3dc53a47e3998395c62f9663f839c4492ba0
unknown <joelaaronross>
2018-10-03 14:04:18 -0700

    added customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,34 @@
 
 public class Customer extends Person {
 
+	private String first;
+	private String last;
+	private String number;
+	
+	
+	public Customer(String first, String last, String number) {
+		super();
+		this.first = first;
+		this.last = last;
+		this.number = number;
+	}
+
+
+	public String getNumber() {
+		return number;
+	}
+
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [first=" + first + ", last=" + last + ", number=" + number + "]";
+	}
+	
+	
+
 }
