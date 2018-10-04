2df9fb78d147c5e0793198adc9d734bd548c7b32
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-10-04 09:49:01 -0400

    Updates to the App

--- src/PersonManagerApp.java
+++ src/PersonManagerApp.java
@@ -2,8 +2,19 @@
 public class PersonManagerApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Person Manager)";
+		System.out.println("Welcome to the Person Manager");
 		System.out.println();
+		
+		String choice = "y";
+		
+		while(choice.equalsIgnoreCase("y")) {
+		
+		choice = Console.getString("Continue (y/n)?", "y", "n");
+
+		}
+
+		System.out.println("Bye!");
+
 	}
 
 }
